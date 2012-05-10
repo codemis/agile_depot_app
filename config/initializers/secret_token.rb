99f564f501f202da4b5584f71b5244db040562ec
Codemis <johnathan@still-water.com>
2012-05-10 07:59:41 -0700

    Initial code.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'e135f7246260c815c832c9f566c90bb3e45a469865866de86c4ca5fc8c77f661babe900f7cea1ef9983a3c9584c88604087f1e1ef92ec88b6c73ed4a01496629'
