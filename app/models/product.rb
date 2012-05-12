cc5cae5ebc3d736a042a1c97a9765b582b9ab789
Codemis <johnathan@still-water.com>
2012-05-12 12:15:54 -0700

    Added validation to the Product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,24 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :image_url, :price, :title
+  # attributes
+	#
+	attr_accessible :description, :image_url, :price, :title
+	
+	# validations
+	#
+	validates :title, :description, :image_url, presence: true
+	
+	# Validate the price is .01 or greater
+	#
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	
+	# validate title is unique
+	#
+	validates :title, uniqueness: true
+	
+	# validate the url for the image
+	#
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)$}i,
+		message: 'must be a URL for GIF, JPG, or PNG image.'
+	}
 end
